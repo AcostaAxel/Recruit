9162d62170e2059339dc0e5d5b8978a453e64854
Axel <axelacosta.tp@gmail.com>
2022-11-04 11:16:33 -0300

    initial commit

---
+++ app/src/main/java/com/vanaco/recruit/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.vanaco.recruit
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            installSplashScreen()
+        }
+    }
+}
---
+++ app/src/main/java/com/vanaco/recruit/ui/theme/Color.kt
@@ -0,0 +1,12 @@
+package com.vanaco.recruit.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val DarkGray = Color(0xFF202020)
+val LightBlue = Color(0xFFD7E8DE)
+
+val Red = Color(0xFFEC6366)
+val Orange = Color(0xFFF6A977)
+val Yellow = Color(0xFFEAE097)
+val Green = Color(0xFFABCD8D)
+val Blue = Color(0xFF6F8AAA)
